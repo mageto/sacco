1b1ae33bc5591c8edf13dd911b47d0cffc928561
mageto <mageto.denis@gmail.com>
2023-07-05 20:03:18 +0300

    SACCO-2 : Added boilerplate for landing page, dashboard and login

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,14 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>SACCO</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
-        <div id="app"></div>
+        <div class="flex justify-center mt-24">
+        <h1 class="text-2xl font-bold text-gray-700">Landing Page Boilerplate</h1>
+    </div>
     </body>
     <script src="{{ asset('js/app.js') }}"></script>
 </html>
